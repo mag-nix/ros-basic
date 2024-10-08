73dfa363d716bad6ba4b52ed491c6c37a06adbe9
Johannes Jeising <jeising@magazino.eu>
2024-10-08 14:13:34 +0200

    extend to basic talker listener example

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Basic ROS Setup
 
-This repo contains a basic ros setup utilizing [lopsided98:nix-ros-overlay]
+This repo contains a basic ros shell setup utilizing [lopsided98:nix-ros-overlay](https://github.com/lopsided98/nix-ros-overlay)
 
-[lopsided98:nix-ros-overlay]: https://github.com/lopsided98/nix-ros-overlay
+## How to use
+
+### Install nix and enable flakes
+
+- [Install Nix](https://nixos.org/nix/download.html)
+- [Enable Flakes](https://wiki.nixos.org/wiki/Flakes)
+- (Optional) [configure Nix to use ROS Cachix binary cache](https://github.com/lopsided98/nix-ros-overlay#configure-binary-cache)
+- Try one of the [examples](https://github.com/lopsided98/nix-ros-overlay#examples)
+
+### Enter nix shell
+
+``` bash
+nix develop
+```
--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
-    nixpkgs.follows = "nix-ros-overlay/nixpkgs";
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs"; # Usage of different nixpkgs for fixes and adaptions
   };
   outputs = { self, nix-ros-overlay, nixpkgs }:
     nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
@@ -14,15 +14,27 @@
         devShells.default = pkgs.mkShell {
           name = "Example project";
           packages = [
-            pkgs.colcon
-            # ... other non-ROS packages
             (with pkgs.rosPackages.noetic; buildEnv {
                 paths = [
-                    ros-core
-                    # ... other ROS packages
+                    # Core ROS Packages
+                    # ros-core
+                    roslaunch
+                    rosbash
+                    rospy-tutorials
                 ];
             })
           ];
+
+          ROS_HOSTNAME = "localhost";
+          ROS_MASTER_URI = "http://localhost:11311";
+
+          shellHook = ''
+            # enable auto completion
+            source ${pkgs.rosPackages.noetic.rosbash}/share/rosbash/rosbash
+            # convenient aliases
+            alias talker='rosrun rospy_tutorials talker'
+            alias listener='rosrun rospy_tutorials listener'
+          '';
         };
       });
   nixConfig = {
